6ca48f174d2387049ff567327dfa014e8bd072a6
Duncan Mortimer <dmortime@thoughtworks.com>
2019-07-26 09:59:47 +0000

    Add jupyter notebook configuration file

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-jupyter notebook --ip=0.0.0.0 --allow-root --no-browser
+jupyter notebook \
+  --ip=0.0.0.0\
+  --allow-root\
+  --no-browser\
+  --config=/workspaces/linear-algebra/config/jupyter_notebook_config.py
